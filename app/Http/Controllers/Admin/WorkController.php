3a3b218af2b83524ecd653beb57a4ac9716cf13a
Pan <s-pengqin.pan@rakuten.com>
2020-10-12 17:31:56 +0900

    12345

--- app/Http/Controllers/Admin/WorkController.php
+++ app/Http/Controllers/Admin/WorkController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Works;
+
 class WorkController extends Controller
 {
 
@@ -15,7 +17,28 @@ class WorkController extends Controller
   // 以下を追記
   public function create(Request $request)
   {
-    // admin/work/createにリダイレクトする
+    
+    // Varidationを行う
+    $this->validate($request, Works::$rules);
+
+    $works = new Works;
+    $form = $request->all();
+
+    // フォームからファイルが送信されてきたら、保存して、$works->file_path に画像のパスを保存する
+    if (isset($form['file'])) {
+      $path = $request->file('file')->store('public/file');
+      $works->file_path = basename($path);
+    } else {
+        $works->file_path = null;
+    }
+
+    unset($form['_token']);
+    unset($form['file']);
+
+    // データベースに保存する
+    $works->fill($form);
+    $works->save();
+
     return redirect('admin/work/create');
   }
   
